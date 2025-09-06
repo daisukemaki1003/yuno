e3744160c548be522d96e25d079462888aa28165
daisukemaki1003 <daisukemaki1003@gmail.com>
2025-09-06 18:46:35 +0900

    TypeScriptを使用した新しいサーバー機能を追加し、`index.js`を削除して`dist`ディレクトリにビルドされたファイルを生成。`tsconfig.json`を新規作成し、依存関係に`typescript`、`nodemon`、`concurrently`を追加。デバッグ用の`launch.json`を作成し、サーバーの起動スクリプトを更新しました。

--- server/index.js
+++
@@ -1,5 +0,0 @@
-import functions from '@google-cloud/functions-framework'
-
-export const helloGET = (req, res) => {
-    res.send('Hello World!');
-});
---
+++ server/src/index.ts
@@ -0,0 +1,6 @@
+import * as ff from "@google-cloud/functions-framework";
+import type { HttpFunction } from "@google-cloud/functions-framework";
+
+export const helloGET: HttpFunction = (req: ff.Request, res: ff.Response) => {
+  res.send(`Hello World! ${req.body}`);
+};
